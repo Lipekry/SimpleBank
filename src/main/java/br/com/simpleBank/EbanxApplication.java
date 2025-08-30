38fbc2e499a2ff204e79116480fccdfa6a7e5ea5
Luis Kryzozun <lipekry@gmail.com>
2025-08-30 20:01:07 -0300

    Inicializando projeto com Spring Boot.

---
+++ src/main/java/br/com/simpleBank/EbanxApplication.java
@@ -0,0 +1,13 @@
+package br.com.simpleBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbanxApplication.class, args);
+	}
+
+}
